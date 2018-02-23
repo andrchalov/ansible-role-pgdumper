eaa71291ea0a470a1e3ade054c711bfd95db7a11
Andrey Chalov <andrchalov@gmail.com>
2018-02-23 16:05:29 +0300

    dump time and size

--- files/pgdumper.py
+++ files/pgdumper.py
@@ -9,6 +9,7 @@ import subprocess
 import urllib
 import urllib.request
 from datetime import datetime
+import time
 
 ###################### functions #####################
 
@@ -17,9 +18,9 @@ def send_message(msg, iserror=False):
     return
 
   if iserror:
-    text = LOG_PREFIX + 'ERROR: ' + msg
+    text = LOG_PREFIX + ' ERROR: ' + msg
   else:
-    text = LOG_PREFIX + msg
+    text = LOG_PREFIX + ' ' + msg
 
   data = {
     "chat_id": TELEGRAM_CHANNEL,
@@ -32,14 +33,36 @@ def send_message(msg, iserror=False):
 
   req = urllib.request.Request(url, data=params, headers={'content-type': 'application/json'})
   response = urllib.request.urlopen(req)
-  # print(response.read())
+
+def convert_bytes(num):
+  """
+  this function will convert bytes to MB.... GB... etc
+  """
+  for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+      if num < 1024.0:
+          return "%3.1f %s" % (num, x)
+      num /= 1024.0
+
+
+def file_size(file_path):
+  """
+  this function will return the file size
+  """
+  if os.path.isfile(file_path):
+      file_info = os.stat(file_path)
+      return convert_bytes(file_info.st_size)
+
+def pretty_time(sec):
+  m, s = divmod(sec, 60)
+  h, m = divmod(m, 60)
+  return "%d:%02d:%02d" % (h, m, s)
 
 ######################################################
+start_time = time.time()
 
 os.chdir('/dumps')
 
 parser = argparse.ArgumentParser(description='Dumps postgresql database.\nAll pg_dump options allowed.', add_help=False)
-parser.add_argument('-d', metavar="DBNAME", required=True, help="database to dump")
 parser.add_argument('-f', metavar="filename", required=True, default='', help="output file or directory name")
 parser.add_argument('-?','--help', action='help', help="show this help, then exit")
 
@@ -51,8 +74,10 @@ LOG_PREFIX = os.environ.get('LOG_PREFIX', 'pgdumper: ')
 
 popen = subprocess.Popen('pg_dump '+' '.join(sys.argv[1:])+' -w', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = popen.communicate()
+
 if popen.returncode != 0:
   send_message(err.decode("utf-8"), iserror=True)
   raise Exception(err)
 else:
-  send_message('database "'+args.d+'" dumped to file: '+args.f)
+  runtime = pretty_time(int(time.time() - start_time))
+  send_message('dump created: '+args.f+' size: '+file_size(args.f)+' time: '+runtime)
